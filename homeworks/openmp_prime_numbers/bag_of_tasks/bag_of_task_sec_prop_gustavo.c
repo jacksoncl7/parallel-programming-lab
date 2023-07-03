50ebab9652dfe637a320259c14aa6616e7702aca
Gustavo Queiroz <gustavohqueiroz22@gmail.com>
2023-07-02 22:42:46 -0300

    new bag of tasks logic

---
+++ homeworks/openmp_prime_numbers/bag_of_tasks/bag_of_task_sec_prop_gustavo.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <omp.h>
+#include <ctype.h>
+#include <time.h>
+
+#define MAX_TASKS 100
+
+int isPrime(int num) {
+    if (num <= 1)
+        return 0;
+
+    if (num == 2)
+        return 1;
+
+    if (num % 2 == 0)
+        return 0;
+
+    for (int i = 3; i * i <= num; i += 2) {
+        if (num % i == 0)
+            return 0;
+    }
+
+    return 1;
+}
+
+bool isInteger(const char *str) {
+    if (str == NULL || *str == '\0' || isspace(*str))
+        return false;
+
+    char *endptr;
+    strtol(str, &endptr, 10);
+
+    return (*endptr == '\0');
+}
+
+bool validateInput(int argc, char *argv[]) {
+    if (argc != 4 || !isInteger(argv[1]) || !isInteger(argv[2]) || !isInteger(argv[3])) {
+        printf("Uso: ./bag_of_tasks <objetivo> <num_threads> <size_of_bag>\n");
+        return false;
+    }
+
+    return true;
+}
+
+int main(int argc, char *argv[]) {
+
+    if (!validateInput(argc, argv)) {
+        return 1;
+    }
+
+    int objective = atoi(argv[1]);
+    int numThreads = atoi(argv[2]);
+    int chunkSize = atoi(argv[3]);
+
+    omp_set_num_threads(numThreads);
+    int start = 3;
+    int end = 0;
+    int primeCount = 0;
+    bool workerControl = true;
+    omp_lock_t generationSem, consumeSem;
+
+    int localCount = 0;
+    int localStart = 0;
+    int localEnd = 0;
+
+    double startingTime = omp_get_wtime();
+    omp_init_lock(&generationSem);
+    omp_init_lock(&consumeSem);
+    // omp_set_lock(&consumeSem);
+
+    #pragma omp parallel private(localCount)
+    {
+        int id = omp_get_thread_num();
+        if(id == 0)
+        {
+            while (end != objective){
+                end = start + chunkSize;
+                if (end > objective) { end = objective; }
+                printf("\nMaster [%d] criou um novo chunk %d..%d\n", omp_get_thread_num(), start, end);
+
+                while(!omp_test_lock(&generationSem)) {
+                    continue;
+                }
+                if (end != objective) start = end + 1;
+                omp_unset_lock(&consumeSem);
+            }
+        }
+
+        else
+        {
+            while (end != objective) {
+                if  (end == 0) continue;
+                {
+                    if(!omp_test_lock(&consumeSem)) continue;
+                    omp_test_lock(&generationSem);
+                    localCount = 0;
+                    for (int num = start; num <= end; num++) {
+                        if (isPrime(num))
+                        {
+                            localCount++;
+                        }
+                    }
+                    primeCount += localCount;
+
+                    printf("\nResultado na thread [%d] com chunk %d..%d foi %d \n", omp_get_thread_num() , start, end, localCount, primeCount);
+                    omp_unset_lock(&generationSem);
+                }
+            }
+        }
+        #pragma omp barrier
+    }
+
+    double finishingTime = omp_get_wtime();
+    primeCount += 1; // Adds the number '2', which is also prime.
+    printf("Quant. de primos entre 1 e n: %ld \n", primeCount);
+	printf("Tempo de execucao: %1.3lf \n", finishingTime - startingTime);
+
+    return 0;
+}
---
+++ homeworks/openmp_prime_numbers/bag_of_tasks/build_gustavo.sh
@@ -0,0 +1,1 @@
+g++ bag_of_task_sec_prop_gustavo.c -fopenmp -o3 -lm -o build/bag_of_task_sec_prop_gustavo
